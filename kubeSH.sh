9d422461c5bb58849a6cbed6384c0d3e916f8b94
hafizhfahss <61222459+hafizhfahss@users.noreply.github.com>
2025-04-16 17:36:14 +0700

    kubeSH.sh

---
+++ kubeSH.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Get the list of pod IPs
+POD_IPS=$(kubectl get pods -o wide --no-headers | awk '{print $6}')
+
+# Loop through each IP and ping
+for IP in $POD_IPS; do
+    echo "Pinging Pod IP: $IP"
+    ping -c 4 $IP || echo "Failed to reach $IP"
+    echo "-----------------------------------"
+done
